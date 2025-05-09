22456746904e96027d13dd70519287d006b42e0f
Mario928 <prashant.shihora@nyu.edu>
2025-05-09 00:52:11 -0400

    Update data.tf and main.tf related to new  new security groups structure

--- tf/kvm/data.tf
+++ tf/kvm/data.tf
@@ -34,28 +34,4 @@ data "openstack_networking_secgroup_v2" "allow_9090" {
   name = "allow-9090"
 }
 
-data "openstack_networking_secgroup_v2" "allow_8001" {
-  name = "allow-8001"
-}
-
-data "openstack_networking_secgroup_v2" "allow_8002" {
-  name = "allow-8002"
-}
-
-data "openstack_networking_secgroup_v2" "allow_8501" {
-  name = "allow-8501"
-}
-
-data "openstack_networking_secgroup_v2" "allow_3000" {
-  name = "allow-3000"
-}
-
-data "openstack_networking_secgroup_v2" "allow_5050" {
-  name = "allow-5050"
-}
-
-data "openstack_networking_secgroup_v2" "allow_8888" {
-  name = "allow-8888"
-}
-
 
